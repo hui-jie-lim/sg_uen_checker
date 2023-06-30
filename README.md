a7cffd50392b286e4d31d6e827d798cd7fd8e27b
huuji <limhj90@gmail.com>
2023-06-30 19:06:05 +0100

    updated code to improve clarity

--- README.md
+++ README.md
@@ -13,3 +13,13 @@ Thereafter run the following to install stdnum onto your local machine. Ideally,
 simply run the following. entries is optional. if not provided, we'll use a default of 100 entries to verify against. 
 
 `python3 checker.py <entries>`
+
+# more info on UENs
+'* R = Registered * D = Deregistered
+Reference table: https://www.uen.gov.sg/ueninternet/faces/pages/admin/aboutUEN.jspx
+
+# sources
+Current source : https://data.gov.sg/dataset/entities-with-unique-entity-number
+Alternate source: https://data.gov.sg/dataset/acra-information-on-corporate-entities
+Alternate source list entities from a - z
+alpha_a = "6b5cbfa7-b502-4ce6-875d-dafff7ff04f2" 
--- checker.py
+++ checker.py
@@ -3,26 +3,52 @@ import requests
 
 from stdnum.sg import uen
 
+# argument processing
 if len(sys.argv) == 1: # default
     limit = 10
-
-if len(sys.argv) == 2: # default
-    if isinstance(sys.argv[1],int) and sys.argv[1] > 0: 
-        limit = sys.argv[1]
-    else:
-        limit = 10
-
-if len(sys.argv) > 2: # use default
-    limit = 10
-
+if len(sys.argv) == 2: 
+    limit = sys.argv[1]
 limit_str = "&limit="+str(limit)
 
-r = requests.get("https://data.gov.sg/api/action/datastore_search?resource_id=6b5cbfa7-b502-4ce6-875d-dafff7ff04f2"+limit_str)
+# source : https://data.gov.sg/dataset/entities-with-unique-entity-number
+acra_resource_id = "39201285-b73e-487a-a971-3a12d34ab8d9"
+other_resource_id = "5ab68aac-91f6-4f39-9b21-698610bdf3f7" 
+acra_errors = 0
+other_errors = 0
+errors = []
+count = 0
 
-companies = list(r.json()["result"]["records"])
-for coy in companies:
-    uen_no = coy["uen"]
-    if (uen.validate(uen_no) == uen_no):
-        continue
+def validate(category_name: str, uen_resource_id: str, count: int):
+    count = 0
+    r = requests.get("https://data.gov.sg/api/action/datastore_search?resource_id="
+                    + uen_resource_id
+                    + limit_str)
+    
+    companies = list(r.json()["result"]["records"])
+    for coy in companies:
+        uen_no = coy["uen"]
+        try:
+            if uen.validate(uen_no) == uen_no:
+                count += 1
+        except:
+            errors.append(tuple([uen_no, coy["issuance_agency_id"], coy["uen_status"], coy["entity_type"]]))    
+ 
+    print("Total", category_name, "registered validated count", count )
+    error_count = len(errors) 
+    if error_count == 0:
+        print("No errors")
+    else:
+        print(error_count, category_name, "UEN with errors")
 
-print("all good for", limit, "checks" )
+    return error_count
+
+
+acra_errors = validate("ACRA", acra_resource_id, count)
+print("")
+other_errors = validate("Other", other_resource_id, count)
+print("")
+print("Total", acra_errors + other_errors, "UEN with errors")
+print(errors)
+
+
+
